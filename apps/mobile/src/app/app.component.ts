648060d1808ad1ca4338969098a69f23ef12980f
Chris Weight <chris@chrisweight.com>
2023-01-30 12:17:36 +0000

    Add initial apps, shared lib structure

---
+++ apps/mobile/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 't-and-t-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  constructor() {}
+}
---
+++ apps/mobile/src/app/home/home.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 't-and-t-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  constructor() {}
+}
